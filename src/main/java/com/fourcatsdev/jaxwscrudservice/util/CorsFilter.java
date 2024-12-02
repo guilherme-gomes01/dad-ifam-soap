0abd359e6a6e4b1ebcd347d5b35b90dff113b57f
Messyas <messyasgoisfranca@gmail.com>
2024-12-02 13:43:30 -0400

    Adicao do CorsFilter

---
+++ src/main/java/com/fourcatsdev/jaxwscrudservice/util/CorsFilter.java
@@ -0,0 +1,43 @@
+package com.fourcatsdev.jaxwscrudservice.util;
+
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+
+@WebFilter("/*") // Aplica o filtro a todas as requisições
+public class CorsFilter implements Filter {
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+    }
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+            throws IOException, ServletException {
+
+        HttpServletResponse res = (HttpServletResponse) response;
+        HttpServletRequest req = (HttpServletRequest) request;
+
+        // Adiciona os cabeçalhos CORS
+        res.setHeader("Access-Control-Allow-Origin", "*");
+        res.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE");
+        res.setHeader("Access-Control-Allow-Headers", "Content-Type, SOAPAction");
+        res.setHeader("Access-Control-Allow-Credentials", "true");
+
+        // Lida com pré-requisições OPTIONS
+        if ("OPTIONS".equalsIgnoreCase(req.getMethod())) {
+            res.setStatus(HttpServletResponse.SC_OK);
+            return;
+        }
+
+        // Continua a cadeia de filtros
+        chain.doFilter(request, response);
+    }
+
+    @Override
+    public void destroy() {
+    }
+}
